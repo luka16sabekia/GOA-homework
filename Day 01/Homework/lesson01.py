21b7f04f977ae7cf83e23e61ff7e40191fb11e45
luka16sabekia <lukasabekia92@gmail.com>
2024-03-20 06:04:33 -0700

    commit

--- Day 01/Homework/lesson01.py
+++ Day 01/Homework/lesson01.py
@@ -2,8 +2,8 @@ name ="luka"
 surname ="sabekia"
 age =14
 height =155
-print(name + surname)
-print(name +str(age))
+print(name +" " +surname)
+print(name +" " + str(age))
 weight=(54)
 origin="Tbilisi"
 hobby="football"
@@ -14,3 +14,4 @@ print(type(height))
 print(type(weight))
 print(type(origin))
 print(type(hobby))
+print(name + surname + age + height + weight + origin + hobby) 
